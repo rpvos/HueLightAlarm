2671a283902d39cef98963acab1660e7de7fba31
Rik <rik.vos01@gmail.com>
2020-11-24 13:37:14 +0100

    [added] weekmodel

--- app/src/main/java/app/com/huelightsalarm/ui/fragments/AlarmCardModel.java
+++ app/src/main/java/app/com/huelightsalarm/ui/fragments/AlarmCardModel.java
@@ -1,17 +1,16 @@
 package app.com.huelightsalarm.ui.fragments;
 
-import java.sql.Time;
 import java.time.LocalDateTime;
 
-import app.com.huelightsalarm.ui.DaysOfWeek;
+import app.com.huelightsalarm.ui.WeekModel;
 
 public class AlarmCardModel {
 
     private LocalDateTime alarmTime;
-    private DaysOfWeek.WeekDays weekDays;
+    private WeekModel.WeekDays weekDays;
     private boolean activated;
 
-    public AlarmCardModel(LocalDateTime alarmTime, boolean activated, DaysOfWeek.WeekDays weekDays) {
+    public AlarmCardModel(LocalDateTime alarmTime, boolean activated, WeekModel.WeekDays weekDays) {
         this.alarmTime = alarmTime;
         this.activated = activated;
         this.weekDays = weekDays;
