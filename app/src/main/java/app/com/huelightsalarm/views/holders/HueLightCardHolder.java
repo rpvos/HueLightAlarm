7c1a6f1a5e25a7520bd9c8ece45fbb3315f5f7fb
stijn <lemmstijn@hotmail.com>
2020-12-03 15:02:49 +0100

    [added] small refactor change

--- app/src/main/java/app/com/huelightsalarm/views/holders/HueLightCardHolder.java
+++ app/src/main/java/app/com/huelightsalarm/views/holders/HueLightCardHolder.java
@@ -1,24 +1,32 @@
 package app.com.huelightsalarm.views.holders;
 
+import android.graphics.Color;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import app.com.huelightsalarm.R;
 
 public class HueLightCardHolder extends RecyclerView.ViewHolder {
 
+    private final CardView cardView;
     private final TextView lightName;
     private final Switch hueLightSwitch;
+    private final ImageView imageView;
 
     public HueLightCardHolder(@NonNull View itemView) {
         super(itemView);
 
         this.lightName = itemView.findViewById(R.id.TextView_HueLight);
         this.hueLightSwitch = itemView.findViewById(R.id.Switch_HueLight);
+        this.cardView = itemView.findViewById(R.id.CardView_HueLamp);
+        this.imageView = itemView.findViewById(R.id.ImageView_HueLight);
+        imageView.setColorFilter(Color.BLUE);
     }
 
     public TextView getLightName() {
@@ -27,5 +35,9 @@ public class HueLightCardHolder extends RecyclerView.ViewHolder {
 
     public Switch getHueLightSwitch() {
         return hueLightSwitch;
+    }
+
+    public void subscribeToOnClick(View.OnClickListener onClickListener) {
+        this.cardView.setOnClickListener(onClickListener);
     }
 }
